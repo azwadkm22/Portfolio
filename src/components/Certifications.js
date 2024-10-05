5bf8038e4362f95167e4730afdf261122609e15a
unknown <azwadkm22@gmail.com>
2024-10-05 20:30:31 +0600

    Added certificate description and links

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -4,34 +4,41 @@ import React from 'react';
 function Certifications() {
     const certifications = [
         {
-            title: 'React - The Complete Guide',
-            provider: 'Udemy',
-            date: 'June 2023',
-            url: 'https://www.udemy.com/certificate/react-course' // Example URL
-        },
-        {
-            title: 'Full-Stack Web Development with Node.js',
-            provider: 'Coursera',
-            date: 'April 2023',
-            url: 'https://www.coursera.org/certificate/web-dev-course' // Example URL
-        },
-        {
-            title: 'Data Science Professional Certificate',
+            title: 'IBM Data Science Professional Certificate',
             provider: 'IBM',
+            date: 'March 2024',
+            url: 'https://coursera.org/share/83e19f6a4d3a1ddbb4f903f523674b5e' // Example URL
+        },
+        
+        {
+            title: 'Principles of UX/UI Design',
+            provider: 'Meta',
+            date: 'March 2023',
+            url: 'https://coursera.org/share/a1d08ab18090f986ac9aa7044d5a4b27' // Example URL
+        },
+        {
+            title: 'Fundamentals of Reinforcement Learning',
+            provider: 'University of Alberta',
             date: 'February 2023',
-            url: 'https://www.ibm.com/certificate/data-science' // Example URL
+            url: 'https://coursera.org/share/a47ef36a9bf2faad71861bc440ce34dc' // Example URL
+        }, 
+        {
+            title: 'C++ Programming for Unreal Game Development Specialization',
+            provider: 'University of Colorado System',
+            date: 'October 2023',
+            url: 'https://coursera.org/share/67a4e9bc663b2de13b8e32ddffa0fa8a' // Example URL
         },
         {
-            title: 'Introduction to Game Development',
-            provider: 'edX',
-            date: 'August 2022',
-            url: 'https://www.edx.org/certificate/game-development' // Example URL
+            title: 'Generative AI for Software Developers Specialization',
+            provider: 'IBM',
+            date: 'January 2024',
+            url: 'https://coursera.org/share/4f0a4f77cae0ced04c23d5f18f53c51a' // Example URL
         },
         {
-            title: 'Android Development Masterclass',
-            provider: 'Udemy',
-            date: 'November 2022',
-            url: 'https://www.udemy.com/certificate/android-dev-course' // Example URL
+            title: 'Generative AI for Data Scientists Specialization',
+            provider: 'IBM',
+            date: 'February 2024',
+            url: 'https://coursera.org/share/a2269d1b95a17d577297726444747b76' // Example URL
         },
         // Add more courses and certifications here
     ];
