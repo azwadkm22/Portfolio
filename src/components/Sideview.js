2dc49fd1bc312fe17037a3cfb394110cb0cc9630
unknown <azwadkm22@gmail.com>
2024-10-26 00:46:48 +0600

    Changed sideview design

--- src/components/Sideview.js
+++ src/components/Sideview.js
@@ -7,117 +7,116 @@ function SideView() {
     return (
         <div className="fixed left-0 top-16 h-[calc(100vh-64px)] bg-gray-100 shadow-md p-6 w-60 space-y-6 flex flex-col justify-between">
             <ul className="space-y-4">
-                <li className="flex items-center space-x-2 py-5">
-                    <FaHome className="text-gray-600" />
+                <li className="relative flex items-center space-x-2 py-5">
                     <Link
                         to="home"
                         spy={true}
                         smooth={true}
                         offset={-70}
                         duration={500}
-                        className="text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
-                        activeClass="text-blue-500 font-bold"
+                        className="flex items-center w-full space-x-2 text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
+                        activeClass="active-link"
                     >
-                        Home
+                        <FaHome className="text-gray-600" />
+                        <span>Home</span>
                     </Link>
                 </li>
-                <li className="flex items-center space-x-2 py-5">
-                    <FaUser className="text-gray-600" />
+                <li className="relative flex items-center space-x-2 py-5">
                     <Link
                         to="about"
                         spy={true}
                         smooth={true}
                         offset={-70}
                         duration={500}
-                        className="text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
-                        activeClass="text-blue-500 font-bold"
+                        className="flex items-center w-full space-x-2 text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
+                        activeClass="active-link"
                     >
-                        About
+                        <FaUser className="text-gray-600" />
+                        <span>About</span>
                     </Link>
                 </li>
-                <li className="flex items-center space-x-2 py-5">
-                    <FaBriefcase className="text-gray-600" />
+                <li className="relative flex items-center space-x-2 py-5">
                     <Link
                         to="experience"
                         spy={true}
                         smooth={true}
                         offset={-70}
                         duration={500}
-                        className="text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
-                        activeClass="text-blue-500 font-bold"
+                        className="flex items-center w-full space-x-2 text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
+                        activeClass="active-link"
                     >
-                        Experience
+                        <FaBriefcase className="text-gray-600" />
+                        <span>Experience</span>
                     </Link>
                 </li>
-                <li className="flex items-center space-x-2 py-5">
-                    <FaTrophy className="text-gray-600" />
+                <li className="relative flex items-center space-x-2 py-5">
                     <Link
                         to="achievements"
                         spy={true}
                         smooth={true}
                         offset={-70}
                         duration={500}
-                        className="text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
-                        activeClass="text-blue-500 font-bold"
+                        className="flex items-center w-full space-x-2 text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
+                        activeClass="active-link"
                     >
-                        Achievements
+                        <FaTrophy className="text-gray-600" />
+                        <span>Achievements</span>
                     </Link>
                 </li>
-                <li className="flex items-center space-x-2 py-5">
-                    <FaCode className="text-gray-600" />
+                <li className="relative flex items-center space-x-2 py-5">
                     <Link
                         to="skills"
                         spy={true}
                         smooth={true}
                         offset={-70}
                         duration={500}
-                        className="text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
-                        activeClass="text-blue-500 font-bold"
+                        className="flex items-center w-full space-x-2 text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
+                        activeClass="active-link"
                     >
-                        Skills
+                        <FaCode className="text-gray-600" />
+                        <span>Skills</span>
                     </Link>
                 </li>
-                <li className="flex items-center space-x-2 py-5">
-                    <FaProjectDiagram className="text-gray-600" />
+                <li className="relative flex items-center space-x-2 py-5">
                     <Link
                         to="projects"
                         spy={true}
                         smooth={true}
                         offset={-70}
                         duration={500}
-                        className="text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
-                        activeClass="text-blue-500 font-bold"
+                        className="flex items-center w-full space-x-2 text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
+                        activeClass="active-link"
                     >
-                        Projects
+                        <FaProjectDiagram className="text-gray-600" />
+                        <span>Projects</span>
                     </Link>
                 </li>
-                
-                <li className="flex items-center space-x-2 py-5">
-                    <FaCertificate className="text-gray-600" />
+                <li className="relative flex items-center space-x-2 py-5">
                     <Link
                         to="certifications"
                         spy={true}
                         smooth={true}
                         offset={-70}
                         duration={500}
-                        className="text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
-                        activeClass="text-blue-500 font-bold"
+                        className="flex items-center w-full space-x-2 text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
+                        activeClass="active-link"
                     >
-                        Certifications
+                        <FaCertificate className="text-gray-600" />
+                        <span>Certifications</span>
                     </Link>
                 </li>
-                <li className="flex items-center space-x-2 py-5">
-                    <FaEnvelope className="text-gray-600 " />
+                <li className="relative flex items-center space-x-2 py-5">
                     <Link
                         to="contact"
                         spy={true}
                         smooth={true}
                         offset={-70}
                         duration={500}
-                        className="text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
-                        activeClass="text-blue-500 font-bold"
+                        className="flex items-center w-full space-x-2 text-gray-600 hover:text-blue-500 transition-all duration-300 cursor-pointer"
+                        activeClass="active-link"
                     >
-                        Contact
+                        <FaEnvelope className="text-gray-600" />
+                        <span>Contact</span>
                     </Link>
                 </li>
             </ul>
