939039d6eb2b808700824563674dd110a554ed9f
unknown <azwadkm22@gmail.com>
2024-11-05 02:29:05 +0600

    Fixed padding issue in Experience section.

--- src/components/ExperienceItem.js
+++ src/components/ExperienceItem.js
@@ -5,9 +5,9 @@ function ExperienceItem({ company, title, date, description }) {
     return (
         <div className="border-l-4 border-blue-500 pl-6 mb-10">
             <h3 className="text-xl md:text-2xl font-semibold text-gray-800">{company}</h3>
-            <p className="text-sm md:text-md text-gray-500 mb-1">{date}</p>
-            <p className="text-sm md:text-xl font-medium text-gray-700">{title}</p>
-            <p className="text-sm md:text-lg text-gray-600 mt-3">{description}</p>
+            <p className="text-sm sm:text-lg text-gray-500 mb-1">{date}</p>
+            <p className="text-sm sm:text-xl font-medium text-gray-700">{title}</p>
+            <p className="text-sm sm:text-lg text-gray-600 mt-3">{description}</p>
         </div>
     );
 }
