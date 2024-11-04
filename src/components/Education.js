4f46e84266a6021873a0f1b8bd884f2bc4efe228
unknown <azwadkm22@gmail.com>
2024-11-05 00:18:51 +0600

    Made the Education section responsive.

--- src/components/Education.js
+++ src/components/Education.js
@@ -33,24 +33,24 @@ function Education() {
     return (
         <div className="bg-gray-100 text-gray-800 py-24">
             <div className="max-w-4xl mx-auto">
-                <h1 className="text-4xl font-bold text-center text-left mb-12">Education</h1>
+                <h1 className="text-2xl md:text-4xl font-bold text-center text-left mb-12">Education</h1>
                 <div className="relative">
                     {/* Timeline Line */}
-                    <div className="absolute border-r-4 border-gray-300 h-full left-16"></div>
+                    <div className="absolute border-r-4 border-gray-300 h-full left-4 md:left-16"></div>
 
                     <div className="space-y-12">
                         {qualifications.map((qual, index) => (
                             <div key={index} className="relative flex items-center justify-between">
                                 {/* Circle Marker */}
-                                <div className="w-6 h-6 rounded-full bg-gray-400 border-4 border-white absolute left-12"></div>
+                                <div className="w-4 h-4 md:w-6 md:h-6 rounded-full bg-gray-400 border-4 border-white absolute left-4 md:left-12"></div>
                                 {/* Qualification Details */}
-                                <div className="ml-16 pl-8">
-                                    <h2 className="text-xl font-semibold">{qual.degree}</h2>
-                                    <p className="text-gray-600">{qual.institution} | {qual.year}</p>
-                                    <p className="text-gray-500 italic">{qual.cgpa}</p>
+                                <div className="ml-6 pl-4 md:ml-16 md:pl-8">
+                                    <h2 className="text-base md:text-xl font-semibold">{qual.degree}</h2>
+                                    <p className="text-sm md:text-base text-gray-600">{qual.institution} | {qual.year}</p>
+                                    <p className="text-sm md:text-base text-gray-500 italic">{qual.cgpa}</p>
                                 </div>
                                 {/* Institution Logo */}
-                                <div className="w-24 h-24 ml-8 flex-shrink-0">
+                                <div className="w-24 h-24 ml-8 flex-shrink-0 invisible md:visible">
                                     <img src={qual.logo} alt={`${qual.institution} logo`} className="object-contain w-full h-full" />
                                 </div>
                             </div>
